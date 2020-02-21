868cebafb642cb39e109df57df5ade1b45b1c0e7
Ubuntu <ubuntu@ip-172-31-92-24.ec2.internal>
2020-02-21 07:31:53 +0000

    sinatra1 exercises

---
+++ sinatra1.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+enable :sessions
+
+get '/getdoc' do
+ "<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: purple;
+}
+
+h1 {
+  color: yellow;
+}
+</style>
+</head>
+<body>
+
+<h1>Zup World!</h1>
+
+</body>
+</html> "
+end
+
+get '/' do
+  "Hello World"
+end
+
+ post "/logon" do
+  if params[:username] == "bob" && params[:password] == "pass"
+      "SUCCESS"
+  else
+      "FAILURE"
+  end
+  end
+
+ post "/hello" do
+  session[:name] = params[:name]
+    "session saved"
+   end
+ 
+ get "/greet" do 
+  "Good evening #{session[:name]}"
+ end
+   
+   
+   
+   
